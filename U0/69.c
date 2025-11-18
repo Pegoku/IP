f8c95e9ceae3d0abb5c02889d667927f23c653f3
Pegoku <pegoku08@gmail.com>
2025-11-18 09:57:08 +0100

    69

--- U0/69.c
+++ U0/69.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 
 int main() {
+    FILE* fptr = fopen("file.txt", "a");
+    char a[128], b[64], c[64], n[4] = {'\n'};
+
+    printf("Introdueix un nombre: ");
+    scanf("%s", b);
+    fputs(b, fptr);
+    fputs(n, fptr);
+    printf("Introdueix una cadena: ");
+    scanf("%s", c);
+    fputs(c, fptr);
     return 0;
 }
