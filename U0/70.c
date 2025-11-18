351c533b81de44c84d726f64b9bf8e3424a0a7ae
Pegoku <pegoku08@gmail.com>
2025-11-18 10:32:26 +0100

    70

--- U0/70.c
+++ U0/70.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
-int calcQuadrat(int num){
-    return num * num;
-}
-
+// frase[strcspn(frase, '\n')]='\0';
 int main() {
-    printf("%d", calcQuadrat(4));
+    FILE* fptr = fopen("file.txt", "r");
+
+    char data[2][70];
+    for (int i = 0; i<2; i++){
+        fgets(data[i], 70, fptr);
+    }
+    fclose(fptr);
+
+    FILE* fptr2 = fopen("file2.txt", "a");
+    char n[2] = {'\n'};
+    for (int i = 1; i>=0; i--){
+        
+        // printf("%s\n", data[i]);
+        fputs(data[i],fptr2);
+        fputs(n,fptr2);
+    }
+    fclose(fptr);
+
     return 0;
 }
