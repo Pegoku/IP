a68c08c976503e53a8c35459dac9fdb810fa2e79
Pegoku <pegoku08@gmail.com>
2025-11-20 12:38:53 +0100

    72

--- U0/71.c
+++ U0/71.c
@@ -5,6 +5,7 @@ int main() {
     FILE* fptr = fopen("file.txt", "w");
 
     for (int i = 0; i<10; i++){
+        printf("Introdueix un nombre: ");
         scanf("%d", &nums[i]);
         nums[i] = nums[i]*5;
         char str[128];
--- U0/72.c
+++ U0/72.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-
-int calcQuadrat(int num){
-    return num * num;
-}
-
+#include <stdlib.h>
+#include <string.h>
 int main() {
-    printf("%d", calcQuadrat(4));
+    char a[128];
+    printf("Introdueix el nom de l'arxiu a emprar: ");
+    fgets(a,128, stdin);
+    strtok(a, '\n');
+    FILE* fptr = fopen(a, "r");
+    char c;
+    do{
+        c = fgetc(fptr);
+    } while(c);
     return 0;
 }
