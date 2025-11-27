d799b3888401ff7cc828117e1e70e9283e518fe0
Pegoku <pegoku08@gmail.com>
2025-11-27 14:03:03 +0100

    76

--- U0/76.c
+++ U0/76.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-int calcQuadrat(int num){
-    return num * num;
-}
+int main()
+{
+    FILE *fptr = fopen("file.txt", "r");
 
-int main() {
-    printf("%d", calcQuadrat(4));
+    int l = 0, v = 0, n = 0, p = 1;
+    while (1)
+    {
+        char a = fgetc(fptr);
+        if (a == ' ')
+            p++;
+
+        if (isalpha(a))
+        {
+            l++;
+            if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' || a == 'A' || a == 'E' || a == 'I' || a == 'O' || a == 'U')
+            {
+                v++;
+            }
+        }
+        if (isdigit(a))
+            n++;
+        if (feof(fptr))
+            break;
+    }
+
+    printf("Lletres: %d, Vocals: %d, Nombres: %d, Paraules: %d", l, v, n, p);
     return 0;
 }
