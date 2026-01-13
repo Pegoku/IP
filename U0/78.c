0c56854d20dc9bcecac10cdd127e66cffe144f22
Pegoku <pegoku08@gmail.com>
2026-01-13 09:22:55 +0100

    WIP 78

--- U0/78.c
+++ U0/78.c
@@ -23,7 +23,7 @@ void clear()
 void createCl()
 {
 
-    char nom[64], llinatge1[64], llinatge2[64], direcció[128], dni[16];
+    char nom[64], llinatge1[64], llinatge2[64], direcció[128], dni[16], ch;
     int tel;
 
     printf("Nom: ");
@@ -45,14 +45,35 @@ void createCl()
 
     clear();
 
-    printf("Nombre de client: %d", numClients);
+    printf("Nombre de client: %d\n", numClients);
     printf("Nom: %s\n", nom);
     printf("Llinatges: %s %s\n", llinatge1, llinatge2);
     printf("Direcció: %s\n", direcció);
     printf("DNI: %s\n", dni);
-    printf("Nombre de telefón: %s\n", tel);
-    // clients[numClients] =
+    printf("Nombre de telefón: %d\n", tel);
+
+    getchar();
+
+    printf("Son aquests valors correctes? (Y/n): ");
+    scanf("%c", &ch);
+    if (ch == 'n') return;
+
+    strcpy(clients[numClients].direcció, direcció);
+    strcpy(clients[numClients].dni, dni);
+    strcpy(clients[numClients].llinatge1, llinatge1);
+    strcpy(clients[numClients].llinatge2, llinatge2);
+    strcpy(clients[numClients].nom, nom);
+    clients[numClients].tel = tel;
+
     numClients++;
+}
+
+void noCl(){
+    char ch;
+    printf("No hi ha clients en el sistema, vols crear-ne un? (y/N) ");
+    scanf("%c", &ch);
+    if (ch == 'y' || ch == 'Y') createCl(); 
+    else clear();
 }
 
 void modCl(int n)
@@ -65,9 +86,74 @@ void delCl(int n)
 
 void viewCl(int n)
 {
+    char ch;
+    printf("Nombre de client: %d", n);
+    printf("Nom: %s\n", clients[n].nom);
+    printf("Llinatges: %s %s\n", clients[n].llinatge1, clients[n].llinatge2);
+    printf("Direcció: %s\n", clients[n].direcció);
+    printf("DNI: %s\n", clients[n].dni);
+    printf("Nombre de telefón: %s\n", clients[n].tel);
+
+    printf("Son aquests valors correctes? (Y/n): ");
+    scanf("%c", &ch);
+    if (ch == 'n') return;
 }
+
+void listCl(){
+    printf("%d", numClients);
+    if (numClients == 0) noCl(); return;
+    for (int i = 0; i<numClients; i++){
+        printf("Client n. %d: %s %s, %s\n", i, clients[i].llinatge1, clients[i].llinatge2, clients[i].nom);
+    }
+}
+
 
 int main()
 {
+    int op = 0;
+
+    while (op != 6){
+        printf("Tria una opció: \n");
+        printf(" (1) Crear usuari\n");
+        printf(" (2) Llista usuaris\n");
+        printf(" (3) Detall usuari\n");
+        printf(" (4) Modificar usuari\n");
+        printf(" (5) Eliminar usuari\n");
+        printf(" (6) Sortir\n");
+        scanf("%d", &op);
+        getchar();
+        
+        switch (op)
+        {
+        case 1:
+            clear();
+            createCl();
+            break;
+        
+        case 2:
+            clear();
+            listCl(); 
+            break;
+        case 3:
+            clear();
+            break;
+        case 4:
+            clear();
+            break;
+        case 5:
+            clear();
+            break;
+        case 6:
+            printf("Exiting...");
+            break;
+        default:
+            printf("Opció no vàlida.");
+            break;
+        }
+
+
+
+    }
     createCl();
+
 }
